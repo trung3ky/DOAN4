b09938c70c4ee60a68317f72038ee9986985a00a
= <=>
2020-12-02 02:06:13 +0700

    tim kiếm bạn bè

--- controller/homeController.js
+++ controller/homeController.js
@@ -2,6 +2,7 @@ var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var flash = require('connect-flash');
 var check = require('../middleware/checksSgin');
+var connection = require('./db');
 module.exports = function(app) {
     app.use(session({
         secret: 'secret',
@@ -13,6 +14,28 @@ module.exports = function(app) {
 
     app.use(cookieParser());
 
-    app.get('/index', check.checkcookie, check.search);
+    app.get('/index', check.checkcookie);
+
+    app.get('/search', function(req, res, next) {
+        var regex = req.query["term"];
+        var searchName = "select * from user where name like '%" + regex + "%'";
+        connection.query(searchName, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            var data = [];
+            for (var i = 0; i < result.length; i++) {
+                var obj = {
+                    name: result[i].name,
+                    image: result[i].image
+                };
+                data.push(obj);
+            }
+            res.send(data, {
+                'Content-Type': 'application/json'
+            }, 200);
+
+        });
+    });
 
 }
